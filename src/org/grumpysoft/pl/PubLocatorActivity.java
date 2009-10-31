5169397250d968c12f5a38ab72a015ffd005b592
grumpyjames <grumpyjames@gmail.com>
2009-10-31 19:00:53 +0000

    Center text, get more than one element correctly aligned

--- src/org/grumpysoft/pl/PubLocatorActivity.java
+++ src/org/grumpysoft/pl/PubLocatorActivity.java
@@ -2,6 +2,8 @@ package org.grumpysoft.pl;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -13,10 +15,15 @@ public class PubLocatorActivity extends Activity {
         setContentView(R.layout.main);
         LinearLayout llv = new LinearLayout(this);
         llv.setOrientation(1);
+        LayoutParams lp = new LayoutParams(LayoutParams.FILL_PARENT, 50);
         TextView one = new TextView(this);
+        one.setLayoutParams(lp);
         one.setText("I am number one");
+        one.setGravity(Gravity.CENTER);
         TextView two = new TextView(this);
+        two.setLayoutParams(lp);
         two.setText("I am number two");
+        two.setGravity(Gravity.CENTER);
         llv.addView(one);
         llv.addView(two);
         setContentView(llv);
