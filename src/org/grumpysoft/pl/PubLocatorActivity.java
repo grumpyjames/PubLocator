2f981b52ed15a1a43568c5fb259c71f628942e81
grumpyjames <grumpyjames@gmail.com>
2009-11-01 12:27:07 +0000

    Decent programmatically instantiated layout with intents

--- src/org/grumpysoft/pl/PubLocatorActivity.java
+++ src/org/grumpysoft/pl/PubLocatorActivity.java
@@ -1,14 +1,9 @@
 package org.grumpysoft.pl;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.text.util.Linkify;
-import android.view.Gravity;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class PubLocatorActivity extends Activity {
     /** Called when the activity is first created. */
@@ -18,22 +13,12 @@ public class PubLocatorActivity extends Activity {
         setContentView(R.layout.main);
         LinearLayout llv = new LinearLayout(this);
         llv.setOrientation(1);
-        LayoutParams lp = new LayoutParams(LayoutParams.FILL_PARENT, 50);
-        TextView one = new TextView(this);
-        one.setLayoutParams(lp);
-        one.setText("I am number one");
-        one.setGravity(Gravity.CENTER);
-        TextView two = new TextView(this);
-        two.setLayoutParams(lp);
-        two.setText("I am number two");
-        two.setAutoLinkMask(Linkify.ALL);
-        two.setGravity(Gravity.CENTER);
-        llv.addView(one);
-        llv.addView(two);
+        LayoutParams lp = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+        llv.setLayoutParams(lp);
+        PubSource ps = new DummyPubSource();
+        for (Pub p: ps.findPubs(23.2,23.2)) {
+        	llv.addView(new PubSummaryLayout(this,p));
+        }
         setContentView(llv);
-        Intent ffs = new Intent();
-        ffs.setAction(Intent.ACTION_VIEW);
-        ffs.setData(Uri.parse("geo:51.4,0.0?z=18"));
-        startActivity(ffs);
     }
 }
--- src/org/grumpysoft/pl/PubSummaryLayout.java
+++ src/org/grumpysoft/pl/PubSummaryLayout.java
@@ -1,36 +1,65 @@
 package org.grumpysoft.pl;
 
 import android.content.Context;
-import android.util.AttributeSet;
-import android.view.Gravity;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public final class PubSummaryLayout extends LinearLayout {
 
+	
 	private static final LayoutParams lp_ = new LayoutParams(LayoutParams.FILL_PARENT, 50);
+	private static final LayoutParams button_lp = new LayoutParams(
+			LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+	static {
+		button_lp.weight = 0;
+	}
 	
-	private final Pub pub_;	
+	private final Context instantiator_;
+	private final Pub pub_;
 	
-	// Common initialization for both constructors
-	private final void init (Context context) {
+	private Intent map_opening_intent_;
+	
+	private final void init () {
 		super.setLayoutParams(lp_);
-		TextView pub_name = new TextView(context);
-		pub_name.setGravity(Gravity.CENTER);
+		TextView pub_name = new TextView(instantiator_);
 		pub_name.setText(pub_.name());
+		LayoutParams pt_lp = new LayoutParams(lp_);
+		pt_lp.weight = 1;
+		pub_name.setLayoutParams(pt_lp);
 		super.addView(pub_name);
+		Button show_review = new Button(instantiator_);
+		show_review.setLayoutParams(button_lp);
+		show_review.setText("R");
+		super.addView(show_review);
+		Button show_on_map = new Button(instantiator_);
+		show_on_map.setLayoutParams(button_lp);
+		show_on_map.setText("M");
+		show_on_map.setOnClickListener(new ShowMapListener());
+		super.addView(show_on_map);
+		map_opening_intent_ = new Intent();
+        map_opening_intent_.setAction(Intent.ACTION_VIEW);
+        StringBuilder sb = new StringBuilder();
+        sb.append("geo:").append(pub_.latitude()).append(",").append(pub_.longitude()).append("?z=18");
+        map_opening_intent_.setData(Uri.parse(sb.toString()));
 	}
 	
 	public PubSummaryLayout(Context context, Pub pub) {
 		super(context);
+		instantiator_ = context;
 		pub_ = pub;
-		init(context);
+		init();
 	}
+	
+	private class ShowMapListener implements View.OnClickListener {
 
-	public PubSummaryLayout(Context context, AttributeSet attrs, Pub pub) {
-		super(context, attrs);
-		pub_ = pub;
-		init(context);
+		public void onClick(View v) {
+			instantiator_.startActivity(map_opening_intent_);
+		}
+	
 	}
 
 }
