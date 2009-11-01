a23b755f17019e6f86c3cabef4f780ab9e09c2d7
james <grumpyjames@gmail.com>
2009-11-01 19:00:20 +0000

    Allow scrolling when layout is big enough

--- src/org/grumpysoft/pl/PubLocatorActivity.java
+++ src/org/grumpysoft/pl/PubLocatorActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class PubLocatorActivity extends Activity implements UpdatableLocation {
@@ -14,32 +15,43 @@ public class PubLocatorActivity extends Activity implements UpdatableLocation {
     private TextView gps_view_;
     @SuppressWarnings("unused")
     private GpsListenerWrapper glw_;
+    private LinearLayout lly_;
 	
     /** Called when the activity is first created. */
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        LinearLayout lly = new LinearLayout(this);
-        lly.setOrientation(1);
-        LayoutParams lp = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
-        lly.setLayoutParams(lp);
+        lly_ = new LinearLayout(this);
+        lly_.setOrientation(1);
+        LayoutParams lp = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
+        lly_.setLayoutParams(lp);
         gps_view_ = new TextView(this);
-        LayoutParams gps_lp = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
+        LayoutParams gps_lp = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         gps_view_.setLayoutParams(gps_lp);
         gps_view_.setGravity(Gravity.CENTER);
         gps_view_.setText("Waiting for gps position...");
-        lly.addView(gps_view_);
+        lly_.addView(gps_view_);
         glw_ = new GpsListenerWrapper(this, this);
-        ps_ = new DummyPubSource();
-        for (Pub p: ps_.findPubs(23.2,23.2)) {
-        	lly.addView(new PubSummaryLayout(this,p));
-        }
-        setContentView(lly);
+        ps_ = new HttpPubSource(new JSONResponseTranslator());
+        lly_.setScrollBarStyle(3);
+        ScrollView sv = new ScrollView(this);
+        sv.addView(lly_);
+        setContentView(sv);
     }
+	
+	@SuppressWarnings("unused")
+	private void reset(LinearLayout pub_holder) {
+		if (pub_holder.getChildCount()>1) {
+			pub_holder.removeViews(1, pub_holder.getChildCount() - 1);
+		}
+	}
 
 	public void update(Location l) {
 		gps_view_.setText(l.getLatitude() + " , " + l.getLongitude());
+		for (Pub p: ps_.findPubs(l.getLatitude(),l.getLongitude())) {
+        	lly_.addView(new PubSummaryLayout(this,p));
+        }
 	}
     
 }
