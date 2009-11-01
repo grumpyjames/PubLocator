32961fa6d50bced521ffb4edf6b1f534c927c3ca
james <grumpyjames@gmail.com>
2009-11-01 18:22:43 +0000

    Change http source to work with API

--- src/org/grumpysoft/pl/HttpPubSource.java
+++ src/org/grumpysoft/pl/HttpPubSource.java
@@ -7,8 +7,6 @@ import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
 
 public class HttpPubSource implements PubSource {
 
@@ -20,13 +18,11 @@ public class HttpPubSource implements PubSource {
 		linguist_ = linguist;
 	}
 	
-	
 	public ArrayList<Pub> findPubs(double latitude, double longitude) {
-		HttpGet httpget = new HttpGet("http://www.digihippo.net/find_pubs");
-        HttpParams params = new BasicHttpParams();
-        params.setDoubleParameter("latitude", latitude);
-        params.setDoubleParameter("longitude", longitude);
-        httpget.setParams(params);
+		StringBuilder sb = new StringBuilder();
+		sb.append("http://foundry.digihippo.net/fancyapint/webroot/Pubs/FindByLocation/");
+		sb.append(latitude).append("/").append(longitude).append("/");
+		HttpGet httpget = new HttpGet(sb.toString());
         ResponseHandler<String> responseHandler = new BasicResponseHandler();
 		String response_body;
         try { 
